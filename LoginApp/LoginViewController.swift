dc3063b1ea751df994f8e1644470e39e6dfabec1
DmKeller <dmit.keller@gmail.com>
2023-03-31 22:30:52 +0300

    Create Interface

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Келлер Дмитрий on 31.03.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+    }
+
+
+}
+
